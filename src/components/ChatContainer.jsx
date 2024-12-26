dc5db0cbb332656b36fd84be5bfc1865bcc828c4
Leticia <leticiavidal2910@gmail.com>
2024-12-26 14:50:35 -0300

    feat: added skeleton to container

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -2,6 +2,7 @@ import { useChatStore } from "../store/useChatStore"
 import { useEffect } from "react"
 import ChatHeader from "./ChatHeader"
 import MessageInput from "./MessageInput"
+import MessageSkeleton from "./skeletons/MessageSkeleton"
 
 
 const ChatContainer = () => {
@@ -10,7 +11,13 @@ const ChatContainer = () => {
     useEffect(() => {
         getMessages(selectedUser._id)
     }, [selectedUser._id, getMessages])
-    if(isMessagesLoading) return <div>Loading ...</div>
+    if(isMessagesLoading) return (
+        <div className="flex-1 flex flex-col overflow-auto">
+            <ChatHeader />
+            <MessageSkeleton />
+            <MessageInput />
+        </div>
+    )
 
   return (
     <div className="flex-1 flex flex-col overflow-auto">
