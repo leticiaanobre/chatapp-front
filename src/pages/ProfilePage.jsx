40ad42072d71aeececbe9d150bd34d7fd276d19b
Leticia <leticiavidal2910@gmail.com>
2024-12-26 12:52:06 -0300

    feat: created edit profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,22 +1,37 @@
-import { Camera } from "lucide-react"
-import { useAuthStore } from "../store/useAuthStore"
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Camera, Mail, User } from "lucide-react";
 
 const ProfilePage = () => {
-  const {authUser, isUpdatingProfile, updateProfile} = useAuthStore()
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+  const [selectedImg, setSelectedImg] = useState(null);
 
-  const handleImageUpload = async(e) => {
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0]; //file that the user selected
+    if (!file) return;
 
-  }
+    const reader = new FileReader(); //render the file and render it on the ui
+
+    reader.readAsDataURL(file);
+
+    reader.onload = async () => { //convert into the base 64 format
+      const base64Image = reader.result;
+      setSelectedImg(base64Image);
+      await updateProfile({ profilePic: base64Image });
+    };
+  };
+
   return (
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
-            <h1 className="text-2xl font-semibold">Perfil</h1>
-            <p>Suas informações</p>
+            <h1 className="text-2xl font-semibold ">Profile</h1>
+            <p className="mt-2">Your profile information</p>
           </div>
 
           {/* avatar upload section */}
+
           <div className="flex flex-col items-center gap-4">
             <div className="relative">
               <img
@@ -46,14 +61,44 @@ const ProfilePage = () => {
               </label>
             </div>
             <p className="text-sm text-zinc-400">
-              {isUpdatingProfile ? "Importando..." : "Clique na câmera para atualizar sua foto"}
+              {isUpdatingProfile ? "Importando..." : "Clique na c^amera para atualizar sua foto"}
             </p>
           </div>
 
+          <div className="space-y-6">
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <User className="w-4 h-4" />
+                Nome completo
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.fullName}</p>
+            </div>
+
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <Mail className="w-4 h-4" />
+                Email
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
+            </div>
+          </div>
+
+          <div className="mt-6 bg-base-300 rounded-xl p-6">
+            <h2 className="text-lg font-medium  mb-4">Account Information</h2>
+            <div className="space-y-3 text-sm">
+              <div className="flex items-center justify-between py-2 border-b border-zinc-700">
+                <span>Member Since</span>
+                <span>{authUser.createdAt?.split("T")[0]}</span>
+              </div>
+              <div className="flex items-center justify-between py-2">
+                <span>Account Status</span>
+                <span className="text-green-500">Active</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
-
-export default ProfilePage
+  );
+};
+export default ProfilePage;
