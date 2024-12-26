70469772f23acafa41a8508dd386e61a6ca136b4
Leticia <leticiavidal2910@gmail.com>
2024-12-26 14:40:32 -0300

    feat: created chatContainer component functions

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,6 +1,15 @@
+import { useChatStore } from "../store/useChatStore"
+import { useEffect } from "react"
 
 
 const ChatContainer = () => {
+    const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore()
+
+    useEffect(() => {
+        getMessages(selectedUser._id)
+    }, [selectedUser._id, getMessages])
+    if(isMessagesLoading) return <div>Loading ...</div>
+
   return (
     <div>ChatContainer</div>
   )
