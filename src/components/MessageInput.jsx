d1278e0d2f1030c22e6946931e8cd8df26d16908
Leticia <leticiavidal2910@gmail.com>
2024-12-26 14:54:21 -0300

    feat: added functions to messageInput

--- src/components/MessageInput.jsx
+++ src/components/MessageInput.jsx
@@ -1,6 +1,10 @@
 
 
 const MessageInput = () => {
+    const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
+  const { sendMessage } = useChatStore();
   return (
     <div>MessageInput</div>
   )
