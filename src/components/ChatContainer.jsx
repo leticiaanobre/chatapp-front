9b4d739c54e65bc450482a577c883bcc10a64f03
Leticia <leticiavidal2910@gmail.com>
2024-12-26 14:44:23 -0300

    feat: adde components to chatcontainer

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,5 +1,7 @@
 import { useChatStore } from "../store/useChatStore"
 import { useEffect } from "react"
+import ChatHeader from "./ChatHeader"
+import MessageInput from "./MessageInput"
 
 
 const ChatContainer = () => {
@@ -11,7 +13,13 @@ const ChatContainer = () => {
     if(isMessagesLoading) return <div>Loading ...</div>
 
   return (
-    <div>ChatContainer</div>
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>messages...</p>
+
+      
+      <MessageInput />
+    </div>
   )
 }
 
