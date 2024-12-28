a7d4767347f9ccbee7579b12a94436e9e3c35975
Leticia <leticiavidal2910@gmail.com>
2024-12-28 17:40:02 -0300

    fix: added socket state

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -93,6 +93,8 @@ export const useAuthStore = create((set, get) => ({
 
         const socket = io(BASE_URL)
         socket.connect()
+
+        set({socket: socket})
     },
     disconnectSocket: () => {
         if(get().socket?.connected) get().socket.disconnect()
