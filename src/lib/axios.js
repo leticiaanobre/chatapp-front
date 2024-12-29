2d385eb1a0db128f4224857601328fd277a9c953
Leticia <leticiavidal2910@gmail.com>
2024-12-28 23:38:13 -0300

    refat: went back to previous url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL,
+    baseURL: "http://localhost:5001/api",
     withCredentials: true //send the cookies in requests
 })
